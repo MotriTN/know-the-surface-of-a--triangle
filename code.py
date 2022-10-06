b7b5def956987aa9a06fb304c76477ec3cd06b93
MotriTN <gamerMT95@gmail.com>
2022-10-06 21:38:24 +0100

    Update code.py

--- code.py
+++ code.py
@@ -1,5 +1,6 @@
-a=int(input("Give a: "))
-b=int(input("Give b: "))
-c=int(input("Give c: "))
+a=float(input("Give a: "))
+b=float(input("Give b: "))
+c=float(input("Give c: "))
 p=(a+b+c)/2
 surf=(p*(p-a)*(p-b)*(p-c))**0.5
+print("Surface: ",surf,"cm²")
